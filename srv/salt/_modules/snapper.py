888364e9f30bd0bf57ea357c131d94a6a0b6c2fb
Pablo Suarez Hernandez <psuarezhernandez@suse.com>
2016-06-06 14:24:44 +0100

    initial snapper.diff command

--- srv/salt/_modules/snapper.py
+++ srv/salt/_modules/snapper.py
@@ -10,6 +10,7 @@ from salt.exceptions import CommandExecutionError
 import logging
 import os
 import time
+import difflib
 from pwd import getpwuid
 
 try:
@@ -123,6 +124,11 @@ def set_config(name='root', **kwargs):
         )
 
 
+def _get_last_snapshot(config='root'):
+    snapshot_list = sorted(list_snapshots(config), key=lambda x: x['id'])
+    return snapshot_list[-1]
+
+
 def get_config(name='root'):
     try:
         config = snapper.GetConfig(name)
@@ -136,6 +142,9 @@ def get_config(name='root'):
 
 def changed_files(config='root', num_pre=None, num_post=None):
     try:
+        num_post = num_post if num_post else _get_last_snapshot(config)['id']
+        num_pre = num_pre if num_pre else int(num_post)-1
+
         snapper.CreateComparison(config, int(num_pre), int(num_post))
         files = snapper.GetFiles(config, int(num_pre), int(num_post))
         return [file[0] for file in files]
@@ -146,3 +155,57 @@ def changed_files(config='root', num_pre=None, num_post=None):
         )
 
 
+def _is_text_file(filename):
+    type_of_file = os.popen('file -bi {0}'.format(filename), 'r').read()
+    return type_of_file.startswith('text')
+
+
+def diff(config='root', filename=None, num_pre=None, num_post=None):
+    '''
+    Returns the differences between two snapshots
+
+    filename
+        if not provided the showing differences between snapshots for
+        all "text" files
+
+    num_pre
+        first snapshot ID to compare. Default is last snapshot ID-1
+
+    num_post
+        last snapshot ID to compare. Default is last snapshot ID
+
+    CLI example:
+
+    .. code-block:: bash
+
+        salt '*' snapper.diff
+        salt '*' snapper.diff filename=/var/log/snapper.log num_pre=19 num_post=20
+    '''
+    try:
+        num_post = num_post if num_post else _get_last_snapshot(config)['id']
+        num_pre = num_pre if num_pre else int(num_post)-1
+
+        files = changed_files(config, num_pre, num_post) if not filename else [filename]
+        pre_mount = snapper.MountSnapshot(config, num_pre, False)
+        post_mount = snapper.MountSnapshot(config, num_post, False)
+
+        files_diff = dict()
+        for f in files:
+            pre_file = pre_mount + f
+            post_file = post_mount + f
+
+            #FIXME: What happends it pre_file or post_file did not exist??? ARGHHH
+            if os.path.isfile(pre_file) and _is_text_file(pre_file):
+                files_diff[f] = ''.join(difflib.unified_diff(open(pre_file).readlines(),
+                                                             open(post_file).readlines(),
+                                                             fromfile=pre_file,
+                                                             tofile=post_file))
+
+        snapper.UmountSnapshot(config, num_pre, False)
+        snapper.UmountSnapshot(config, num_post, False)
+        return files_diff
+    except dbus.DBusException as exc:
+        raise CommandExecutionError(
+            'Error encountered while showing differences between snapshots: {0}'
+            .format(_dbus_exception_to_reason(exc))
+        )
