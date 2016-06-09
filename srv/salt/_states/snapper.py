407d3b7434de52a92fe315c0b1602909b20d76a6
Pablo Suarez Hernandez <psuarezhernandez@suse.com>
2016-06-09 10:10:09 +0100

    snapper state output normalization

--- srv/salt/_states/snapper.py
+++ srv/salt/_states/snapper.py
@@ -133,32 +133,32 @@ def baseline_snapshot(name, number=None, config='root', ignore=[]):
         elif os.path.isdir(f):
             [status.pop(x, None) for x in status.keys() if x.startswith(f)]
 
-
-    # Only include changes for modified files
     for f in status:
-        status[f]['action'] = status[f].pop("status")
-        if "modified" in status[f]['action']:
-            status[f]['diff'] = __salt__['snapper.diff'](config,
-                                                         num_pre=0,
-                                                         num_post=number, filename=f)
+        status[f]['actions'] = status[f].pop("status")
 
-    if status:
-        ret['changes']['files'] = status
-    else:
-        ret['comment'] = "No changes were done"
-
-    if not __opts__['test'] and status.keys():
-        undo = __salt__['snapper.undo'](config, num_pre=number, num_post=0,
-                                        files=status.keys())
-        ret['changes']['sumary'] = undo
+        # Only include diff for modified files
+        if "modified" in status[f]['actions']:
+            status[f].update(__salt__['snapper.diff'](config,
+                                                      num_pre=0,
+                                                      num_post=number, filename=f)[f])
 
     if __opts__['test'] and status:
         ret['pchanges'] = ret["changes"]
         ret['changes'] = {}
         ret['comment'] = "{0} files changes are set to be undone".format(len(status.keys()))
+        ret['result'] = None
     elif __opts__['test'] and not status:
-        ret['result'] = None if not status else True
         ret['changes'] = {}
         ret['comment'] = "Nothing to be done"
+        ret['result'] = True
+    elif not __opts__['test'] and status:
+        undo = __salt__['snapper.undo'](config, num_pre=number, num_post=0,
+                                        files=status.keys())
+        ret['changes']['sumary'] = undo
+        ret['changes']['files'] = status
+        ret['result'] = True
+    else:
+        ret['comment'] = "No changes were done"
+        ret['result'] = True
 
     return ret
